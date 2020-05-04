898f2ebf6407c520d4933caa4bd94ee348a22b22
GonzaloPF <32138290+GonzaloPF@users.noreply.github.com>
2020-05-04 12:25:03 +0000

    predict new values per day by removing outliers

---
+++ python/covid19LinearRegression_outliers.py
@@ -0,0 +1,117 @@
+'''
+Created on Dec 24, 2019
+
+@author: pgonzalo
+'''
+
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.functions import desc
+import numpy as np
+import time
+import datetime
+from scipy import stats
+
+
+sc = SparkContext()
+spark = SparkSession(sc)
+
+
+def dateToUnix(old_date):
+    #transform each date to UNIX format
+    if old_date == "" or old_date == " " or old_date == None:
+        return 0.0
+    da = old_date.split("-")
+    year = da[0].replace(" ", '')
+    day = da[2]
+    month = da[1]
+    date_new = year+"/"+month+"/"+day
+    final_date = time.strptime(date_new, "%Y/%m/%d")
+    d = datetime.date(final_date.tm_year, final_date.tm_mon, final_date.tm_mday)
+    return time.mktime(d.timetuple())
+#group by values
+def groupByProv(val):
+    return (val.cod_ine,[val.fecha,val.total])
+def groupByProv_now(val):
+    return (val.cod_ine,val.total)
+
+#remove total CCAA = 00
+def filterTotal(x):
+    if str(x[0]) == "00":
+        return False
+    return True
+
+
+def linearReg(x):
+    #calculate linear regression by CCAA given a date
+    data = list(x[1])
+    code = []
+    date = []
+    casos = []
+    caso_old = 0
+    #calculate the cases per day, not the acumulated ones
+    for item in data:
+        code.append(x[0])
+        date.append(float(dateToUnix(item[0])))
+        
+        if not caso_old == 0:
+            caso = float(item[1]) - caso_old
+            caso_old = float(item[1])
+            casos.append(caso)
+        else:
+            casos.append(float(item[1]))
+            caso_old = float(item[1])
+    
+    
+    x_pred = np.array(date).reshape(-1, 1)
+    y_pred = np.array(casos).reshape(-1, 1)
+    #calculate outliers
+    z = np.abs(stats.zscore(y_pred))
+    
+    outliers = np.where(z > 1)
+    
+    delete = [num for num in outliers[0]]
+    #remove outliers
+    y_pred_new = np.delete(y_pred, delete, axis=0).reshape(1, -1).tolist()[0]
+    x_pred_new = np.delete(x_pred, delete, axis=0).reshape(1, -1).tolist()[0]
+    
+    #create the model with the values
+    model = np.polyfit(x_pred_new, y_pred_new, 1)
+    predict = np.poly1d(model)
+    new_date = dateToUnix("2020-06-25") #date you want to predict
+    prediction = predict(new_date)
+    
+    #return the value
+    return (x[0], float(prediction))
+      
+def greaterThan(x,y):
+    #get the greater value
+    if int(x) > int(y):
+        return x
+    return y
+
+
+#read the amount of cases
+df_casos = spark.read.load("dataset/covid19casos.csv", format="csv", sep=",", header="True", emptyValue='')
+
+#RDD with the current values: get the greater value by CCAA
+end_casos_now = df_casos.rdd.map(lambda x: groupByProv_now(x)) \
+                        .filter(lambda x: filterTotal(x)) \
+                        .reduceByKey(lambda x,y: greaterThan(x,y)) \
+                        .map(lambda x: (x[0],x[1]))
+#RDD make predictions
+end_casos = df_casos.rdd.map(lambda x: groupByProv(x)) \
+                        .filter(lambda x: filterTotal(x)) \
+                        .groupByKey().map(lambda x: linearReg(x))
+
+
+#join current values with prediction ones
+end = end_casos.join(end_casos_now).map(lambda x: (x[0],x[1][0],x[1][1])).toDF()
+
+end = end.withColumn('cod_prov', end["_1"].cast("int")) 
+end = end.withColumn('prediction', end["_2"].cast("float")) 
+end = end.withColumn('last_day_total', end["_3"].cast("float"))
+end = end.drop(*["_1","_2","_3"]) 
+#show the values
+end.show()
